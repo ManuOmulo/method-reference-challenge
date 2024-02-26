bb3ed2e3e64b6313a2d36481f06a4ea432484c1c
ManuOmulo <omuloemmanuel@gmail.com>
2024-02-26 23:57:40 +0300

    created names and functions array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.UnaryOperator;
+
 public class Main {
+  static Random random = new Random();
+
   public static void main(String[] args) {
+    List<String> firstNames = new ArrayList<>(List.of("Anna", "Bob", "charles", "michael", "David", "Mercy", "don"));
+
+    List<UnaryOperator<String>> functions = new ArrayList<>();
+    functions.add(String::toUpperCase);
+    functions.add(s -> {
+      String lastName = "%s.".formatted(random.nextInt(65, 91));
+      return "%s %s".formatted(s, lastName);
+    });
+    functions.add(s -> {
+      StringBuilder myString = new StringBuilder(s);
+      myString.reverse();
+      return "%s".formatted(myString);
+    });
 
   }
 }
