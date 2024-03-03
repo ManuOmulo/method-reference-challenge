6df543160a8b82e4c2e9eb02f3879e3faf33c85f
ManuOmulo <omuloemmanuel@gmail.com>
2024-03-03 19:51:21 +0300

    used replaceAll method instead of forEach in execute function

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.function.UnaryOperator;
@@ -7,29 +8,34 @@ public class Main {
   static Random random = new Random();
 
   public static void main(String[] args) {
-    List<String> firstNames = new ArrayList<>(List.of("Anna", "Bob", "charles", "michael", "David", "Mercy", "don"));
+    String[] firstNames = {"Anna", "Bob", "charles", "michael", "David", "Mercy", "don"};
 
     List<UnaryOperator<String>> functions = new ArrayList<>();
     functions.add(String::toUpperCase);
+
     functions.add(s -> {
-      String lastName = "%s.".formatted(random.nextInt(65, 91));
+      String lastName = "%c.".formatted(random.nextInt(65, 91));
       return "%s %s".formatted(s, lastName);
     });
+
     functions.add(s -> {
-      StringBuilder myString = new StringBuilder(s);
+      String[] names = s.split(" ");
+      StringBuilder myString = new StringBuilder(names[0]);
       myString.reverse();
-      return "%s".formatted(myString);
+      return "%s %s".formatted(s, myString);
     });
 
     execute(firstNames, functions);
 
   }
 
-  public static void execute(List<String> names, List<UnaryOperator<String>> functions) {
+  public static void execute(String[] names, List<UnaryOperator<String>> functions) {
+    List<String> newList = Arrays.asList(names);
+
     for (var function : functions) {
-      names.forEach(function::apply);
+      newList.replaceAll(name -> name.transform(function));
     }
 
-    System.out.print(names);
+    System.out.println(newList);
   }
 }
